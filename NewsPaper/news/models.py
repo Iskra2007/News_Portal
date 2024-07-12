2e3217053833c9595f3d2c43aedde93140745658
Iskra1107 <118080210+Iskra2007@users.noreply.github.com>
2024-07-12 15:07:33 +0300

    daadw

--- NewsPaper/news/models.py
+++ NewsPaper/news/models.py
@@ -1,30 +1,67 @@
-from datetime import datetime
 from django.db import models
-from django.db import models
+from django.contrib.auth.models import User
 
 
 class Author(models.Model):
-    full_name = models.CharField(max_length=255)
-    bio = models.TextField(default="Информация об авторе")
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    rating = models.IntegerField(default=0)
+
+    def update_rating(self):
+        post_rating = 0
+        post_comment_rating = 0
+        comment_rating = 0
+        for post in self.post_set.all():
+            post_rating += post.rating * 3
+            for comment in post.comment_set.all():
+                if comment.user == self.user:
+                    comment_rating += comment.rating
+                post_comment_rating += comment.rating
+
+        self.rating = post_rating + post_comment_rating + comment_rating
+        self.save()
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField(default="Описание категории")
+    name = models.CharField(max_length=255, unique=True)
 
 
-class Article(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField()
+class Post(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
-    categories = models.ManyToManyField(Category)
-    publication_date = models.DateTimeField(auto_now_add=True)
-    is_published = models.BooleanField(default=False)
+    article_or_news = models.CharField(max_length=10, choices=[('article', 'Article'), ('news', 'News')])
+    post_date = models.DateTimeField(auto_now_add=True)
+    category = models.ManyToManyField(Category, through='PostCategory')
+    title = models.TextField(max_length=100)
+    text = models.TextField()
+    rating = models.IntegerField(default=0)
 
-    def finish_publication(self):
-        self.publication_date = datetime.now()
-        self.is_published = True
+    def preview(self):
+        return self.text[:124] + "..."
+
+    def like(self):
+        self.rating += 1
         self.save()
 
-    def get_prescription(self):
-        return (datetime.now() - self.publication_date).days
+    def dislike(self):
+        self.rating -= 1
+        self.save()
+
+
+class PostCategory(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    post_date = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
